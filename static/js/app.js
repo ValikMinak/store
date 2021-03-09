a7c2466d6a7145c178b6c63e591b49d3309233f6
ValikMinak <valikminak@gmail.com>
2021-03-09 12:49:49 +0200

    Remove static path from urls

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+
+
+$(function (){
+    $('.banner__section--slider').slick({
+        dots:true,
+        prevArrow:'<button class="banner__section--slider-btn banner__section--slider-btnprev"> < </button>',
+        nextArrow:'<button class="banner__section--slider-btn banner__section--slider-btnnext"> > </button>',
+    });
+})
--- store/settings/production.py
+++ store/settings/production.py
@@ -138,12 +138,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = BASE_DIR / 'static/'
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'promotions/static',
-#     BASE_DIR / 'items/static'
-# ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
